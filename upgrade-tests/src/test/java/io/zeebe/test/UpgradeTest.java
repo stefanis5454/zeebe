83ea3b355aed11146b5436ecc114adab35066c19
Miguel Pires <miguel.pires@camunda.com>
2020-02-25 12:40:50 +0100

    chore(upgrade-tests): cover start message event

--- upgrade-tests/src/test/java/io/zeebe/test/UpgradeTest.java
+++ upgrade-tests/src/test/java/io/zeebe/test/UpgradeTest.java
@@ -86,6 +86,14 @@ public class UpgradeTest {
                 .afterUpgrade(UpgradeTest::publishMessage)
                 .done()
           },
+          {
+            "message start event",
+            scenario()
+                .deployWorkflow(msgStartWorkflow())
+                .beforeUpgrade(UpgradeTest::startMsgSubscription)
+                .afterUpgrade(UpgradeTest::publishMessage)
+                .done()
+          }
         });
   }
 
@@ -176,6 +184,19 @@ public class UpgradeTest {
     state.client().newPublishMessageCommand().messageName(MSG).correlationKey("123").send().join();
 
     TestUtil.waitUntil(() -> state.hasMessageInState(MSG, "PUBLISHED"));
+  }
+
+  private static BpmnModelInstance msgStartWorkflow() {
+    return Bpmn.createExecutableProcess(PROCESS_ID)
+        .startEvent()
+        .message(b -> b.zeebeCorrelationKey("key").name(MSG))
+        .endEvent()
+        .done();
+  }
+
+  private static long startMsgSubscription(final ContainerStateRule state) {
+    TestUtil.waitUntil(() -> state.hasLogContaining("MESSAGE_START_EVENT_SUBSCRIPTION", "OPENED"));
+    return -1L;
   }
 
   private static TestCaseBuilder scenario() {
