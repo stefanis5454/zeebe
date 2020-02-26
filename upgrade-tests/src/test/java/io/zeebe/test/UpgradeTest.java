fdef91df755b9be99c5119f076abf7ddaba1114a
Miguel Pires <miguel.pires@camunda.com>
2020-02-26 18:16:03 +0100

    chore(upgrade-tests): cover message records

--- upgrade-tests/src/test/java/io/zeebe/test/UpgradeTest.java
+++ upgrade-tests/src/test/java/io/zeebe/test/UpgradeTest.java
@@ -17,6 +17,7 @@ import io.zeebe.test.util.TestUtil;
 import io.zeebe.util.VersionUtil;
 import java.io.File;
 import java.nio.file.Paths;
+import java.time.Duration;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Map;
@@ -79,7 +80,7 @@ public class UpgradeTest {
                 .done()
           },
           {
-            "message",
+            "message subscription",
             scenario()
                 .deployWorkflow(msgWorkflow())
                 .createInstance()
@@ -110,6 +111,15 @@ public class UpgradeTest {
                 .createInstance()
                 .beforeUpgrade(UpgradeTest::createIncident)
                 .afterUpgrade(UpgradeTest::resolveIncident)
+                .done()
+          },
+          {
+            "publish message",
+            scenario()
+                .deployWorkflow(msgWorkflow())
+                .createInstance()
+                .beforeUpgrade(UpgradeTest::messageSubscription)
+                .afterUpgrade(UpgradeTest::publishMessage)
                 .done()
           }
         });
@@ -201,7 +211,14 @@ public class UpgradeTest {
 
   private static void publishMessage(
       final ContainerStateRule state, final long wfInstanceKey, final long key) {
-    state.client().newPublishMessageCommand().messageName(MSG).correlationKey("123").send().join();
+    state
+        .client()
+        .newPublishMessageCommand()
+        .messageName(MSG)
+        .correlationKey("123")
+        .timeToLive(Duration.ofMinutes(5))
+        .send()
+        .join();
 
     TestUtil.waitUntil(() -> state.hasMessageInState(MSG, "PUBLISHED"));
   }
